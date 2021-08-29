7e31e3a4bb9d971af8aa9e95590bc45c59dbd546
Yakushko Yelena <82969672+yelena-create@users.noreply.github.com>
2021-08-29 20:00:30 +0300

    добавила примеры 3 -4

--- js/02-массивы и функции/02-2-payment.js
+++ js/02-массивы и функции/02-2-payment.js
@@ -7,11 +7,11 @@ const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
 let total = 0;
 
 // 1 перебрать массив
-// for (let i = 0; i < cart.length; i += 1) {
-//   // console.log(cart[i]);
+for (let i = 0; i < cart.length; i += 1) {
+  console.log(cart[i]);
 //   // 3 каждый элемент приплюслвать к total
-//   total += cart[i];
-//   // total = total + cart[i];
+  total += cart[i];
+  total = total + cart[i];
 // }
 
 for (const value of cart) {
@@ -20,7 +20,7 @@ for (const value of cart) {
 
 console.log('Total: ', total);
 
-// Добавить такс
+// Добавить такс(накинуть процент на каждого)
 
 // for (let i = 0; i < cart.length; i += 1) {
 //   cart[i] = Math.round(cart[i] * 1.1);
